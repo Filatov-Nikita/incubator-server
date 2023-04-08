e5e033bd72a972afb6a9b2d3b0dbf3a5955dbb74
Nikita Antonov <nikita45454@gmail.com>
2023-04-08 15:22:02 +0500

    feat: делает email уникальным

--- app/models/user.js
+++ app/models/user.js
@@ -9,6 +9,7 @@ export default (sequlize, models) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
