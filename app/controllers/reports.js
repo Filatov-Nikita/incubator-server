9f87575904a73d2d9c8dea187042545574413fae
Nikita Antonov <nikita45454@gmail.com>
2023-04-15 21:48:30 +0500

    feat: добавляет фильтр в отчет по продажам

--- app/controllers/reports.js
+++ app/controllers/reports.js
@@ -1,11 +1,16 @@
-import { ProductModel, OrderProductModel } from '#app/globals/models.js';
+import { ProductModel, OrderProductModel, OrderModel } from '#app/globals/models.js';
 import { sequelize } from '#app/globals/sequelize.js';
-import { Op, Sequelize, QueryTypes } from 'sequelize';
+import { Op } from 'sequelize';
 
 export async function totalProducts(req, res, next) {
   try {
     const where = {};
-    const { dateFrom, dateTo } = req.query;
+    const productWhere = {};
+    const { dateFrom, dateTo, categoryId } = req.query;
+
+    if(categoryId) {
+      productWhere.categoryId = categoryId;
+    }
 
     if(dateFrom) {
       if(!where.createdAt) where.createdAt = {};
@@ -19,21 +24,44 @@ export async function totalProducts(req, res, next) {
       where.createdAt[Op.lt] = dt;
     }
 
-    const list = await sequelize.query(`
-      SELECT products.id, products.name, OrderProducts.productPrice,
-      SUM(productCount) as count, SUM(productCount * productPrice) as totalSum
-      from OrderProducts JOIN products on OrderProducts.productId = products.id
-      GROUP BY productId, productPrice
-    `, { type: QueryTypes.SELECT });
+    const list = await OrderModel.findAll({
+      attributes: [
+        [sequelize.col('products.name'), 'name'],
+        [sequelize.col('products.id'), 'productId'],
+        [sequelize.col('products.OrderProduct.productPrice'), 'price'],
+        [sequelize.fn('sum', sequelize.col('products.OrderProduct.productCount')), 'count'],
+      ],
+      include: {
+        model: ProductModel,
+        attributes: [],
+        where: productWhere,
+        through: {
+          attributes: []
+        }
+      },
+      where,
+      raw: true,
+      group: [ sequelize.col('products.OrderProduct.productId'), sequelize.col('products.OrderProduct.productPrice') ]
+    });
+
+    let total = 0;
+    list.forEach(item => {
+      const { price, count } = item;
+      item.total = price * count
+      total += item.total
+    });
 
     const resposne = list.map(el => ({
-      product: { id: el.id, name: el.name },
-      totalSum: el.totalSum,
-      productPrice: el.productPrice,
+      product: { id: el.productId, name: el.name },
+      totalSum: el.total,
+      productPrice: el.price,
       count: el.count
     }))
 
-    res.json(resposne);
+    res.json({
+      products: resposne,
+      total
+    });
   } catch(e) {
     next(e);
   }
